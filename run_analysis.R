2fbdad8575b79df7fe1c36cd2d1cb43c373dd1f4
jy-datasci <john.ylioja@gmail.com>
2015-11-15 00:34:03 -0600

    completed steps 1-3

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,40 @@
 #Getting and Cleaning Data, course 3 in the Data Science specialization, course project
+setwd("UCI HAR Dataset")
+print(getwd())
 
-# import and merge data
+# step 1. import and merge data
+# import data
+test <- read.table("test/x_test.txt",comment.char="")
+subTest <- read.table("test/subject_test.txt",comment.char="") #1 column data frame
+yTest <- read.table("test/y_test.txt",colClasses="character",comment.char="")
+# number of columns in test data before adding subject and activity
+nColumns <- dim(test)[2]
+test$subject <- subTest[,1] #extract the data as a vector
+test$activity <- yTest[,1] #extract the data as a vector
+train <- read.table("train/x_train.txt",comment.char="")
+yTrain<-read.table("train/y_train.txt",colClasses="character",comment.char="")
+subTrain<-read.table("train/subject_train.txt",comment.char="") # one column data frame
+train$subject<-subTrain[,1] #extract the data as a vector
+train$activity <- yTrain[,1] #extract the data as a vector
+activities <- read.table("activity_labels.txt",colClasses="character",
+        comment.char="")
+#merge data
+data <- rbind(test, train)
+# step 2. Extract means and standard deviations
+features <- read.table("features.txt",colClasses=c("integer","character"),
+                comment.char="")
+#features[,2] contains the column names.  Ad subject and activity column
+#names.
+columnNames <- c(features[,2],"subject","activity")
+#use grep to find column names which contain mean() or std()
+subsetColumns <- grep("mean()|std()",columnNames)
+#add the subject and activity columns,which have indexes nColumns+1 and nColumns+2
+subsetColumns <- c(subsetColumns, nColumns+1, nColumns+2)
+subsetData <- data[,subsetColumns]
+#change the numbers in the activity column to their labels
+for (i in activities[,1]){
+        data$activity[data$activity==i] <- activities[i,2]
+}
+# make valid r column names
+setwd("..")
+print(getwd())
